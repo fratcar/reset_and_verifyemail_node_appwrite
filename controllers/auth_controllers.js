6f0a38d674893c3f35e68b970a8bd3ae87403e84
fratcar <fratcarcovoit@gmail.com>
2025-01-24 21:37:00 +0100

    Update auth_controllers.js

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -2,7 +2,7 @@ import {Client,Account,ID,Functions} from "appwrite"
 
 const client = new Client()
 .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-.setProject('66f4ad57002d8bb0bf87')              // Your project ID
+.setProject('66f4ad57002d8bb0bf87')  // Your project ID
 
 const account = new Account(client);
 
