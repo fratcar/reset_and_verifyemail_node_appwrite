f13103477524bfecc7f17655f4c9b2f671fc5b6d
fratcar <fratcarcovoit@gmail.com>
2025-01-28 03:19:11 +0100

    Update auth_controllers.js

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -1,17 +1,20 @@
 import {Client,Account,ID,Functions} from "appwrite"
 
-const client = new Client()
-.setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-.setProject('<YOUR-PROJECT-ID>')              // Your project ID
-
-const account = new Account(client);
+  // Supabase initilization
+  await Supabase.initialize(
+    url: "https://hzmnbpmlaltuhbzxvvsb.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6bW5icG1sYWx0dWhienh2dnNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwNDA5NTYsImV4cCI6MjA0MjYxNjk1Nn0.gZi961CtITlgEYL5BIDw6P0iKXrhgOItZFVWoT1dMdc",
+  );
 
 // complete verification for the client
-export const updateVerification = async (userId, secret) => {
+export const updateVerification = async (userEmail, secret) => {
     try{
-       const response=await account.updateVerification(userId, secret);
-         console.log(response); // Success
-            return response;
+      await _supabase.auth.resend(
+        type: OtpType.signup,
+        email: userEmail,
+      );
+     console.log(response); // Success
+        return response;
     }
     catch(error){
         console.log(error); // Failure
